bda397acae0607911f88a0822a68fb0ef80d9e9f
Jit Changdar <jitchangdar7@gamil.com>
2026-04-11 21:10:48 +0530

    update

--- Zira-Frontend/utils/apiurl.js
+++ Zira-Frontend/utils/apiurl.js
@@ -3,12 +3,13 @@ const BASE_URL =
     ? "http://localhost:5000"
     : import.meta.env.VITE_API_URL;
 
-const signup_api = "http://localhost:5000/signup-post-user";
-const user_fetch_api = "http://localhost:5000/api/users/fetch-userid";
-const Signin_api = "http://localhost:5000/quickauthapi/signin";
-const crete_oganization = "http://localhost:5000/api/v1/organizations";
-const send_invite_api = "http://localhost:5000/api/send-invite";
-const refresh_token = "http://localhost:5000/refresh-token";
+const signup_api = `${BASE_URL}/signup-post-user`;
+const user_fetch_api = `${BASE_URL}/api/users/fetch-userid`;
+const Signin_api = `${BASE_URL}/quickauthapi/signin`;
+const crete_oganization = `${BASE_URL}/api/v1/organizations`;
+const send_invite_api = `${BASE_URL}/api/send-invite`;
+const refresh_token = `${BASE_URL}/refresh-token`;
+
 const token = localStorage.getItem("AUTH-X");
 
 export default {
