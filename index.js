e519db512e2375ebf79593904090abbda59277ea
Jit Changdar <jitchangdar7@gamil.com>
2026-04-12 14:17:30 +0530

    cokkie was saved

--- index.js
+++ index.js
@@ -33,11 +33,6 @@ const PORT = process.env.PORT || 5000;
 const saltRounds = 10;
 const secret = process.env.SECRET_KEY;
 
-app.get("/test", (req, res) => {
-  console.log("TEST HIT");
-  res.json({ ok: true });
-});
-
 const verifyToken = (req, res, next) => {
   const token = req.headers["x-auth"];
   if (!token) {
@@ -192,10 +187,10 @@ app.post("/refresh-token", async (req, res) => {
       { expiresIn: "28d" },
     );
 
-    res.cookie("refreshToken", newRefreshToken, {
+      res.cookie("refreshToken", newRefreshToken, {
       httpOnly: true,
-      secure: true, // ✅ changed
-      sameSite: "none", // ✅ changed
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
       maxAge: 28 * 24 * 60 * 60 * 1000,
     });
 
