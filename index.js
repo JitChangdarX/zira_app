bcbdfbf42a428789462dce424134d05d7f9ac64a
Jit Changdar <jitchangdar7@gamil.com>
2026-04-12 13:10:56 +0530

    fix options wildcard using regex

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(cookieParser());
 await connectDB();
 const PORT = process.env.PORT || 5000;
